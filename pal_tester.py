b548e2ae7635f282daa90f2ef4eefb7b85df3993
ashishg12 <46959799+ashishg12@users.noreply.github.com>
2019-01-23 17:16:47 +0530

    Add files via upload

---
+++ pal_tester.py
@@ -0,0 +1,30 @@
+def test_suite():
+    for tst, exp in test_inputs.items():
+        actual = check_palindrome(tst)
+        if actual == exp:
+            print("OK")
+        else:
+            print("NOK")
+
+def check_palindrome(string):
+    """
+        This checks if the given input string is a palindrome
+        It returns True if the input string is a palindrome
+        It returns False if the input string is not a palindrome
+    """
+	if string == string[::-1]:
+		return True # to indicate that the input string is a palindrome
+	return False
+
+# Main test cases
+test_inputs = \
+    {
+        "radar" : True, # test string : expected status
+        "panama" : False,
+        "Madman" : False,
+        "TCATGAACGTCTTCTGCAAGTACT" : True,
+        "GACATACTCCTCCACCTCATACAG" : False,
+    }
+
+
+test_suite()
